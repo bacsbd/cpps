9d9e5905e52a42713f55624030481393bfc4eed6
Mohammad Samiul Islam <forthright48@gmail.com>
2016-08-24 19:59:01 +0600

    Replace mongoose promise with es6 promise

--- configuration/database.js
+++ configuration/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dburl = require('../secret.js').dburl;
 
+mongoose.Promise = global.Promise;
 mongoose.connect(dburl, function(err) {
   if (err) console.log(err);
   else console.log('Successfully connected to database');
