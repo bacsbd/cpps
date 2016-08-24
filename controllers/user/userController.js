88336d1d21104a9f63b556f3004b94e8e70782d1
Mohammad Samiul Islam <forthright48@gmail.com>
2016-08-24 20:19:57 +0600

    Add route and logic for login

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const myRender = require('forthright48/world').myRender;
+const User = require('mongoose').model('User');
 
 const router = express.Router();
 
 router.get('/login', get_login);
+router.post('/login', post_login);
+
+router.get('/test', function(req, res) {
+  res.send(JSON.stringify(req.flash('test')));
+})
 
 module.exports = {
   addRouter(app) {
@@ -16,4 +22,27 @@ module.exports = {
  */
 function get_login(req, res) {
   return myRender(req, res, 'user/login');
+}
+
+function post_login(req, res, next) {
+  const email = User.normalizeEmail(req.body.email);
+  const password = req.body.password;
+
+  User.findOne({
+      email: email
+    })
+    .exec(function(err, user) {
+      if (err) return next(err);
+      if (!user) {
+        req.flash('error', 'Email address not found');
+        return res.redirect('/user/login');
+      }
+      if (user.comparePassword(password)) {
+        req.flash('success', 'Successfully logged in');
+        return res.redirect('/');
+      } else {
+        req.flash('error', 'Password did not match');
+        return res.redirect('/user/login');
+      }
+    });
 }
