6f45d7812340150633669da1e9a8c60666c2e3ee
Mohammad Samiul Islam <forthright48@gmail.com>
2016-08-30 21:59:17 +0600

    Add logic for editing note

--- controllers/notebook/noteController.js
+++ controllers/notebook/noteController.js
@@ -11,6 +11,8 @@ const router = express.Router();
 router.get('/', get_index);
 router.get('/add-note', rootMiddleware, get_addNote);
 router.post('/add-note', rootMiddleware, post_addNote);
+router.get('/edit-note/:slug', get_editNote_Slug);
+router.post('/edit-note/:slug', post_editNote_Slug);
 
 module.exports = {
   addRouter(app) {
@@ -29,7 +31,7 @@ function get_addNote(req, res) {
   return myRender(req, res, 'notebook/addNote');
 }
 
-function post_addNote(req, res) {
+function post_addNote(req, res, next) {
   const note = new Notebook({
     title: req.body.title,
     slug: req.body.slug,
@@ -38,12 +40,69 @@ function post_addNote(req, res) {
 
   note.save(function(err) {
     if (err) {
-      req.flash('error', 'Some error occured');
-    } else {
-      req.flash('success', 'Saved successfully');
+      if (err.code === 11000) {
+        req.flash('error', 'Slug name already exists');
+      } else {
+        req.flash('error', `Some error with code ${err.code}`);
+      }
+      req.flash('context', req.body);
+      return res.redirect('/notebook/add-note');
     }
-
+    req.flash('success', 'Saved successfully');
     req.flash('context', req.body);
     return res.redirect(`/notebook/edit-note/${req.body.slug}`);
+  });
+}
+
+function get_editNote_Slug(req, res, next) {
+  const pslug = req.params.slug;
+
+  Notebook.findOne({
+    slug: pslug
+  }).exec(function(err, note) {
+    if (err) {
+      return next(err);
+    }
+    return myRender(req, res, 'notebook/editNote', {
+      title: note.title,
+      slug: note.slug,
+      body: note.body
+    });
+  });
+}
+
+function post_editNote_Slug(req, res, next) {
+  const pslug = req.params.slug;
+
+  const {
+    title,
+    slug,
+    body
+  } = req.body;
+
+  Notebook.findOne({
+    slug: pslug
+  }).exec(function(err, note) {
+    if (err) {
+      return next(err);
+    }
+    note.title = title;
+    note.slug = slug;
+    note.body = body;
+
+    note.save(function(err) {
+      if (err) {
+        if (err.code === 11000) {
+          req.flash('error', 'Slug name already exists');
+          req.body.slug = pslug;
+        } else {
+          req.flash('error', `Some error with code ${err.code}`);
+        }
+      } else {
+        req.flash('success', 'Edited successfully');
+      }
+      req.flash('context', req.body);
+      return res.redirect(`/notebook/edit-note/${req.body.slug}`);
+    });
   });
 }
