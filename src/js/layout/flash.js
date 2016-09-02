9d9c6a7d983ee9b1f6c46ad766bc26e16182b648
Mohammad Samiul Islam <forthright48@gmail.com>
2016-09-02 19:33:09 +0600

    Make js for layout more modular

--- src/js/layout/flash.js
+++ src/js/layout/flash.js
@@ -1,13 +1,12 @@
-const $ = require('jquery');
-const notify = require('notifyjs-browser')(this, $);
+module.exports = function($) {
+  $.notify.defaults({
+    autoHideDelay: 15000
+  });
 
-$.notify.defaults({
-  autoHideDelay: 15000
-});
-
-for (const val in flash) {
-  const len = flash[val].length;
-  for (let i = 0; i < len; i++) {
-    $.notify(flash[val][i], val);
+  for (const val in flash) {
+    const len = flash[val].length;
+    for (let i = 0; i < len; i++) {
+      $.notify(flash[val][i], val);
+    }
   }
 }
---
+++ src/js/layout/index.js
@@ -0,0 +1,5 @@
+const $ = require('jquery');
+const notify = require('notifyjs-browser')(this, $);
+
+/*Add flash messages*/
+require('./flash')($);
