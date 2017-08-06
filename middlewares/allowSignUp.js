609bb6c2892af3cdadfd1354c8dbca1551a48eba
Mohammad Samiul Islam <forthright48@gmail.com>
2017-08-06 22:51:58 +0600

    Added dev env in compose.yml

--- middlewares/allowSignUp.js
+++ middlewares/allowSignUp.js
@@ -3,8 +3,8 @@
  * If process.env.NO_SIGN_UP is defined, then registration is switched off
  */
 module.exports = function(req, res, next) {
-  //if (process.env.NO_SIGN_UP === "1") {
-  if (true) { // TODO: create configuration file
+  if (process.env.NO_SIGN_UP === "1") {
+  // if (true) { // TODO: create configuration file
     req.flash("info", "Sign up is currently switched off. You need an invitation.");
     return res.redirect("/");
   }
