a8d8b1dbad3bb6706f6df7dfa978d780d5f378ad
Mohammad Samiul Islam <forthright48@gmail.com>
2017-09-15 13:18:55 +0600

    Add more autofill from links

--- src/js/gateway/addItem/index.js
+++ src/js/gateway/addItem/index.js
@@ -36,17 +36,35 @@ function showFormParts() {
 }
 
 function autoFillFromLink(){
+  $('#platform').val(undefined);
+  $('#pid').val('');
+
   const val = $('#link').val();
 
-  const uvaRegExp = RegExp("^https?://uva.onlinejudge.org", "gi");
-  const cfRegExp = RegExp("^https?://codeforces.com/problemset/problem/([\\d]*)/([A-Z])", "gi");
+  const uvaRegExp = /^https?:\/\/uva\.onlinejudge\.org/gi;
+  const cfRegExp = /^https?:\/\/codeforces\.com\/problemset\/problem\/([\d]*)\/([A-Z])/gi;
+  const lojRegExp = /^https?:\/\/www\.lightoj\.com\/volume_showproblem\.php\?problem=([\d]{4,4})/gi;
+  const spojRegExp = /^https?:\/\/www\.spoj\.com\/problems\/([\w]*)/gi;
+  const hduRegExp = /^https?:\/\/acm\.hdu\.edu\.cn\/showproblem\.php\?pid=([\d]{4,4})/gi;
+  const pojRegExp = /https?:\/\/poj\.org\/problem\?id=([\d]{4})/gi;
 
   let arr;
   if (arr = uvaRegExp.exec(val)) {
-    $('select').val('UVa');
+    $('#platform').val('UVa');
   } else if ( arr = cfRegExp.exec(val) ) {
-    $('select').val('CF');
-    console.log(arr, arr[1], arr[2]);
+    $('#platform').val('CF');
     $('#pid').val(arr[1]+arr[2]);
+  } else if ( arr = lojRegExp.exec(val) ) {
+    $('#platform').val('LOJ');
+    $('#pid').val(arr[1]);
+  } else if ( arr = spojRegExp.exec(val) ) {
+    $('#platform').val('SPOJ');
+    $('#pid').val(arr[1]);
+  } else if ( arr = hduRegExp.exec(val) ) {
+    $('#platform').val('HDU');
+    $('#pid').val(arr[1]);
+  } else if ( arr = pojRegExp.exec(val) ) {
+    $('#platform').val('POJ');
+    $('#pid').val(arr[1]);
   }
 }
