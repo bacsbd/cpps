92ca4c68a09bbc9624a62a37ee67a725365cf87e
nuronial_block <ahnaf93@gmail.com>
2017-10-28 18:12:24 +0600

    Implement Classroom schema; closes #55

---
+++ models/classroom.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const classSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Email not valid'
+    },
+    maxlength: 256
+  },
+  students: [{
+    id: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  name: {
+    type: String,
+    require: true,
+    maxlength: 20
+  }
+})
