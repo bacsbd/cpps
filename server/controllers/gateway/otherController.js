91988bd48673e6672d61e208c7a8439ff0b095d7
Mohammad Samiul Islam <forthright48@gmail.com>
2017-12-12 19:17:59 +0600

    Update doneList of problems on a click from dashboard

--- server/controllers/gateway/otherController.js
+++ server/controllers/gateway/otherController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Gate = require('mongoose').model('Gate');
 const User = require('mongoose').model('User');
+const _ = require('lodash');
 
 const router = express.Router();
 
@@ -39,13 +40,24 @@ async function usersThatSolved(ojname, problemId) {
         },
       },
     }, {username: 1}).exec();
-    console.log(userList.map((x)=> x.username));
     return userList.map((x)=> x.username);
   } catch (err) {
     throw err;
   }
 }
 
-function syncProblems(req, res) {
-  usersThatSolved('uva', '100');
+async function syncProblems(req, res) {
+  req.flash('info', 'Processing the request. It will take a while.');
+  res.redirect('/admin/dashboard');
+
+  const problems = await Gate.find({
+    type: 'problem',
+  }, {platform: 1, pid: 1}).exec();
+
+  _.each(problems, async function(p) {
+    // Get users that solved this problem
+    const doneList = await usersThatSolved(p.platform, p.pid);
+    // Update doneList
+    await Gate.findOneAndUpdate({_id: p._id}, {$set: {doneList}});
+  });
 }
